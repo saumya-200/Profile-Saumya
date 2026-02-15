8954a3ffb8545088a8c1dff06b9499207c617e2c
saumya-200 <powtoon0987@gmail.com>
2026-02-15 12:17:48 +0530

    Refactor: Move 3D app to root entry point, removed legacy landing page

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, '3d/index.html'),
-            },
-        },
-    },
-    server: {
-        open: '/3d/',
-    }
 })
